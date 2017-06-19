98d9834388724ef10c056612f31fa4d1b04ab449
Rushma Bajracharya <bajracharya@klimakoenner.de>
2017-06-19 15:31:12 +0200

    styled todo application

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -8,61 +8,68 @@ var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-	getInitialState: function () {
-		return {
-			showCompleted: false,
-			searchText: '',
-			todos: TodoAPI.getTodos()
-		};
-	},
-	componentDidUpdate: function (prevProps, prevState) {
-		TodoAPI.setTodos(this.state.todos);
-	},
-	handleAddTodo: function (text) {
-		this.setState({
-			todos: [
-				...this.state.todos,
-				{
-					id: uuid(),
-					text: text,
-					completed: false,
-					createdAt: moment().unix(),
-					completedAt: undefined
-				}
-			]
-		});
-	},
-	handleToggle: function (id) {
-		var updatedTodos = this.state.todos.map((todo) => {
-			if (todo.id === id) {
-				todo.completed = !todo.completed;
-				todo.completedAt = todo.completed ? moment().unix() : undefined;
-			}
+  getInitialState: function () {
+    return {
+      showCompleted: false,
+      searchText: '',
+      todos: TodoAPI.getTodos()
+    };
+  },
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
+  },
+  handleAddTodo: function (text) {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ]
+    });
+  },
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
+      }
 
-			return todo;
-		});
+      return todo;
+    });
 
-		this.setState({todos: updatedTodos});
-	},
- 	handleSearch: function (showCompleted, searchText){
-		this.setState({
-			showCompleted: showCompleted,
-			searchText: searchText.toLowerCase()
-		});
-	},
-	render: function () {
-		var {todos, showCompleted, searchText} = this.state;
-		var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+    this.setState({todos: updatedTodos});
+  },
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
+  render: function () {
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
-		return (
-			<div>
-				<TodoSearch onSearch={this.handleSearch}/>
-				<TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
-				<AddTodo onAddTodo={this.handleAddTodo}/>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <h1 className="page-title">Todo App</h1>
 
+        <div className="row">
+          <div className="column small-centered small-11 medium-6 large-5">
+            <div className="container">
+              <TodoSearch onSearch={this.handleSearch}/>
+              <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+              <AddTodo onAddTodo={this.handleAddTodo}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 });
 
 module.exports = TodoApp;
--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -2,23 +2,28 @@ var React = require('react');
 var Todo = require('Todo');
 
 var TodoList = React.createClass({
-	render: function () {
-		var {todos} = this.props;
-		var renderTodos = () => {
-			return todos.map ((todo) => {
-				return (
-					<Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>
-				);
-			});
-		};
+  render: function () {
+    var {todos} = this.props;
+    var renderTodos = () => {
+      if (todos.length === 0) {
+        return (
+          <p className="container__message">Nothing To Do</p>
+        );
+      }
 
-		return (
-			<div>
-				{renderTodos()}
-			</div>
-		);
-	}
+      return todos.map((todo) => {
+        return (
+          <Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>
+        );
+      });
+    };
 
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    )
+  }
 });
 
 module.exports = TodoList;
