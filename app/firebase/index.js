577718263466657022bd4f908437c22063e2349d
Rushma Bajracharya <bajracharya@klimakoenner.de>
2017-07-04 15:42:01 +0200

    moved firebase credentials to environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,10 @@ import firebase from 'firebase';
 
 try {
    var config = {
-    apiKey: "AIzaSyDxa7SrA0-DE7bAxjH47_TrC3KZF1-pZDI",
-    authDomain: "todoapp-16f28.firebaseapp.com",
-    databaseURL: "https://todoapp-16f28.firebaseio.com",
-    projectId: "todoapp-16f28",
-    storageBucket: "todoapp-16f28.appspot.com",
-    messagingSenderId: "806036534646"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET
   };
   firebase.initializeApp(config);
 } catch (e) {
